cf57d8636515a39393fff62986155a7c814c3ac4
Blayze <blayzestone@protonmail.com>
2020-06-17 12:33:55 -0700

    test: repair method returns correct value

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,17 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const enhancer = require("./enhancer.js");
+
+describe("enhancer", () => {
+  it("should run tests using it()", () => {
+    expect(true).toBe(true);
+  });
+
+  describe("repair()", () => {
+    const item = {
+      durability: 0,
+    };
+    const actual = enhancer.repair(item);
+    const expected = 100;
+
+    expect(actual.durability).toBe(expected);
+  });
+});
